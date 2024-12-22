161bfacb18b0ee1d37792be496acb50e1fb8fef1
Fahim Muntasir <fahimmuntasir201@gmail.com>
2024-12-22 16:26:04 +0600

    appBar

--- lib/resourse/widget/expenses.dart
+++ lib/resourse/widget/expenses.dart
@@ -33,6 +33,15 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('Chart'),

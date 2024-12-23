61e7bcbe4f149c33145d12325fa4ad51da2cdc56
Fahim Muntasir <fahimmuntasir201@gmail.com>
2024-12-23 18:33:07 +0600

    style

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,38 @@
 import 'package:expense_tracker/resourse/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
+var colorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: colorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: colorScheme.onPrimaryContainer,
+          foregroundColor: colorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: colorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: colorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: colorScheme.onSecondaryContainer,
+                fontSize: 22,
+              ),
+            ),
+      ),
       home: Expenses(),
     ),
   );

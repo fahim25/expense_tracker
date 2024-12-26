8adc13210a3c0b789807ca227f10c0c3209d9903
Fahim Muntasir <fahimmuntasir201@gmail.com>
2024-12-26 11:04:22 +0600

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:expense_tracker/resourse/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
-var colorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var darkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: darkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          color: darkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: darkColorScheme.primaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: colorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -33,6 +54,7 @@ void main() {
               ),
             ),
       ),
+      themeMode: ThemeMode.system, //default
       home: Expenses(),
     ),
   );
